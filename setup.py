0c2a216c28bd22235e237825c49508af7dc18563
Justin Geeslin <justin@geesl.in>
2024-11-26 22:51:14 -0600

    Adding a package script(?)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="clodo-tiles-sensecam",  # Your module name
+    version="0.1.0",  # Your module version
+    description="Tiles for CloDo : SenseCam Edition",  # Short description
+    author="Justin Geeslin",
+    author_email="justin@geesl.in",
+    url="https://github.com/your-username/my_module",
+    packages=find_packages(),  # Automatically finds packages in your directory
+    install_requires=[],  # List dependencies here, if any
+)
